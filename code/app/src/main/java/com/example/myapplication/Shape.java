bca8a1214afd0a5460f48f8e58df2b916d1b7225
HiguchiGenmei <BronyaZaychik19cex@gmail.com>
2025-01-29 10:58:04 +0800

    add color

--- code/app/src/main/java/com/example/myapplication/Shape.java
+++ code/app/src/main/java/com/example/myapplication/Shape.java
@@ -1,11 +1,8 @@
 package com.example.myapplication;
-
 import kotlin.text.UStringsKt;
-
 public abstract class Shape {
     Integer x;
     Integer y;
-
     String color = "blue";
     Shape(Integer x, Integer y) {
         this.x = x;
