e7ad173fb8b11aa2bb949c39fa1b68e1b947f325
Vaibhav Jain <vaibh123540@Vaibhavs-MacBook-Air.local>
2025-01-28 20:02:38 -0700

    implementing color blue

--- code/app/src/main/java/com/example/myapplication/Shape.java
+++ code/app/src/main/java/com/example/myapplication/Shape.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 public abstract class Shape {
     Integer x;
     Integer y;
+
     String color = "blue";
     Shape(Integer x, Integer y) {
         this.x = x;
