db2135b480fe1c80c2b86882e36bdd069a1e9159
garry mand <garrymand@garrys-MacBook-Air.local>
2025-01-28 19:44:14 -0700

    Implementing lab

--- code/app/src/main/java/com/example/myapplication/Shape.java
+++ code/app/src/main/java/com/example/myapplication/Shape.java
@@ -3,9 +3,10 @@ package com.example.myapplication;
 public abstract class Shape {
     Integer x;
     Integer y;
-
+    String color = "blue"
     Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;
+
     }
 }
