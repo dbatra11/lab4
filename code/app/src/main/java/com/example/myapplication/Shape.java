3b88abb6ad48b18b6fb1d01379d4549a76641ce8
dbatra11 <dbatra@ualberta.ca>
2025-01-28 19:06:23 -0700

    Created the Shape abstract class

---
+++ code/app/src/main/java/com/example/myapplication/Shape.java
@@ -0,0 +1,11 @@
+package com.example.myapplication;
+
+public abstract class Shape {
+    Integer x;
+    Integer y;
+
+    Shape(Integer x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
+}
